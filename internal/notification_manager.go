ed3e383062f109cf456b4e91208a8cf5a43baafd
serj <srdjan@adjust.com>
2020-04-18 13:06:28 +0200

    ping pong v1

--- internal/notification_manager.go
+++ internal/notification_manager.go
@@ -7,6 +7,20 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
+)
+
 type Signal struct{}
 
 var EmptySignal = Signal{}
@@ -15,6 +29,7 @@ type NotificationManager struct {
 	db           BuddyDb
 	stopWorkChan chan Signal
 	wsClients    map[*websocket.Conn]bool
+	pongWait     time.Duration // time allowed to read the next pong message from the client
 }
 
 func NewNotificationManager(db BuddyDb) *NotificationManager {
@@ -22,23 +37,31 @@ func NewNotificationManager(db BuddyDb) *NotificationManager {
 		db:           db,
 		stopWorkChan: make(chan Signal, 1),
 		wsClients:    make(map[*websocket.Conn]bool),
+		pongWait:     60 * time.Second,
 	}
 
 	go func() {
+		ticker := time.NewTicker(pingPeriod)
+		defer func() {
+			ticker.Stop()
+		}()
 		for {
 			select {
-			case <-time.After(1 * time.Minute):
+			case <-ticker.C:
 				if len(nm.wsClients) > 0 {
 					log.Warnf("scanning %d clients for dead ws connections ...", len(nm.wsClients))
 				}
-
 				for c, _ := range nm.wsClients {
-					_ = c
-					// TODO:
-					//if closeErr := connClient.Close(); closeErr != nil {
-					//	log.Errorf("cannot close ws client conn: %s", closeErr.Error())
-					//}
-					//delete(rm.wsClients, connClient)
+					if err := c.SetWriteDeadline(time.Now().Add(writeWait)); err != nil {
+						log.Errorf("failed to SetWriteDeadline: %s", err.Error())
+						log.Warnf("closing client conn %s", c.RemoteAddr())
+						delete(nm.wsClients, c)
+					}
+					if err := c.WriteMessage(websocket.PingMessage, nil); err != nil {
+						log.Errorf("failed to write ping message: %s", err.Error())
+						log.Warnf("closing client conn %s", c.RemoteAddr())
+						delete(nm.wsClients, c)
+					}
 				}
 			}
 		}
@@ -51,16 +74,28 @@ func (rm *NotificationManager) NewClient(connClient *websocket.Conn) {
 	rm.wsClients[connClient] = true
 	log.Debugf("notification manager got new client, total: %d", len(rm.wsClients))
 
+	connClient.SetPongHandler(func(string) error {
+		if err := connClient.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
+			log.Errorf("failed to SetReadDeadline: %s", err.Error())
+		}
+		return nil
+	})
+
+	err := connClient.WriteMessage(websocket.TextMessage, []byte("hi from TB server ;)"))
+	if err != nil {
+		log.Errorf("failed to send init message to client %s: %s", connClient.RemoteAddr())
+	}
+
 	go func() {
 		for {
-			err := connClient.WriteMessage(websocket.TextMessage, []byte("hi from TB server ;)"))
-			if err != nil {
-				log.Errorf("failed to send init message to client %s: %s", connClient.RemoteAddr())
-			}
-
 			log.Tracef("waiting for messages from conn client: %s", connClient.RemoteAddr())
 			msgType, message, err := connClient.ReadMessage()
 			if err != nil {
+				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+					log.Tracef("client %s going away (probably)", connClient.RemoteAddr())
+					delete(rm.wsClients, connClient)
+					break
+				}
 				log.Errorf("notification manager read message error: %s", err.Error())
 				break
 			}
